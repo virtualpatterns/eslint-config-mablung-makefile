dbba597580268e4a49336efca3a4da7f8dab7ce5
virtualpatterns.com <code@virtualpatterns.com>
2021-07-31 22:58:41 -0400

    post-update

--- source/test/index.test.cjs
+++ source/test/index.test.cjs
@@ -3,5 +3,5 @@ import Test from 'ava'
 import Configuration from '../index.cjs'
 
 Test('index.cjs', (test) => {
-  test.deepEqual(Configuration.env, { 'es6': true, 'node': true })
+  test.truthy(Configuration)
 })

d64da16cb6a5822d4205fd77049f366ea3d60f6b
virtualpatterns.com <code@virtualpatterns.com>
2021-08-15 13:35:43 -0400

    updates

--- source/test/index.test.js
+++ source/test/index.test.js
@@ -5,6 +5,24 @@ import Test from 'ava'
 const { 'ESLint': Lint } = ESLint
 const Require = CreateRequire(import.meta.url)
 
+Test('arrow-parens', async (test) => {
+
+  let code = 'console.log((b => b + 1)(42)) // eslint-disable-line no-console'
+  let option = {
+    'extends': Require.resolve('../index.cjs')
+  }
+
+  let lint = new Lint({ 'baseConfig': option })
+  let result = await lint.lintText(code)
+
+  test.is(result.length, 1)
+  test.is(result[0].messages.length, 1)
+
+  let message = result[0].messages[0]
+  test.is(message.ruleId, 'arrow-parens')
+
+})
+
 Test('no-console', async (test) => {
 
   let code = 'console.log(42)'
@@ -20,23 +38,5 @@ Test('no-console', async (test) => {
 
   let message = result[0].messages[0]
   test.is(message.ruleId, 'no-console')
-
-})
-
-Test('arrow-parens', async (test) => {
-
-  let code = 'console.log((b => b + 1)(42)) // eslint-disable-line no-console'
-  let option = {
-    'extends': Require.resolve('../index.cjs')
-  }
-  
-  let lint = new Lint({ 'baseConfig': option })
-  let result = await lint.lintText(code)
-
-  test.is(result.length, 1)
-  test.is(result[0].messages.length, 1)
-
-  let message = result[0].messages[0]
-  test.is(message.ruleId, 'arrow-parens')
 
 })

a064cbb41754645e04b21e388b2e410a46259323
virtualpatterns.com <code@virtualpatterns.com>
2021-08-15 00:41:56 -0400

    updates

---
+++ source/test/dependency.test.js
@@ -0,0 +1,27 @@
+import Check from 'depcheck'
+import Test from 'ava'
+
+const Process = process
+
+Test('(default)', async (test) => {
+
+  let unused = await Check(Process.cwd(), {
+    'ignoreMatches': [
+      '@babel/preset-env',
+      'babel-plugin-add-header-comment'
+    ],
+    'parsers': {
+      '**/*.cjs': [ Check.parser.es6, Check.parser.es7.default ],
+      '**/*.js': [ Check.parser.es6, Check.parser.es7.default ]
+    }
+  })
+
+  test.deepEqual(unused.dependencies, [])
+  test.deepEqual(unused.devDependencies, [])
+  
+  test.deepEqual(unused.missing, {})
+
+  test.deepEqual(unused.invalidDirs, {})
+  test.deepEqual(unused.invalidFiles, {})
+
+})
